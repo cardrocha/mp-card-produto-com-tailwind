ace6cca7350c5898fb3d6c56405feef1de9c5a1e
cardrocha <cardrocharj@gmail.com>
2025-03-03 14:31:28 -0300

    feat: update CardProduct styling for improved responsiveness and visual consistency

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className="max-w-[988px] min-h-[803] mx-auto py-11 px-56">
+      <div className="max-w-[988px] min-h-[803] mx-auto py-0 px-0 xl:py-11 xl:px-56">
         <CardProduct />
       </div>
     </>
--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -9,13 +9,13 @@ export function CardProduct() {
 	return (
 		<div className="w-full drop-shadow-2xl">
 			<img
-				className="rounded-tl-2xl rounded-tr-2xl"
+				className="w-full rounded-t-2xl"
 				src={cardProduct}
 				alt="Foto de um produto - tênis"
 			/>
-			<div className="flex flex-col gap-7 p-15 border-b-gray-200 border-l-gray-200 border-r-gray-200 rounded-bl-2xl rounded-br-2xl shadow-2xl">
-				<h1 className="text-5xl font-bold">Nike air Max 90</h1>
-				<p className="text-xl font-medium">
+			<div className="flex flex-col gap-7 p-5 xl:p-15 border-b-gray-200 border-l-gray-200 border-r-gray-200 rounded-b-2xl shadow-2xl">
+				<h1 className="text-4xl md:text-5xl font-bold">Nike air Max 90</h1>
+				<p className="text-lg md:text-xl font-medium">
 					Eleve seu estilo com o clássico reinventado. Conforto supremo encontra
 					design inovador
 				</p>
@@ -35,13 +35,13 @@ export function CardProduct() {
             />
           );
         })}
-        <span className="text-xl text-gray-400">({rating}) (42 avaliações)</span>
+        <span className="text-lg md:text-xl text-gray-400">({rating}) (42 avaliações)</span>
       </div>
 				<div className="flex items-center justify-between"> 
 					<span className="text-4xl text-blue-400 font-bold">R$ 899,00</span>
           <button className="text-xl text-white py-3.5 px-4 bg-linear-to-r from-sky-500 to-indigo-500 rounded-4xl p-1 hover:brightness-125 cursor-pointer" type="button">Comprar agora</button>
 				</div>
-        <p className="flex gap-3.5 text-gray-400">
+        <p className="flex gap-3.5 text-lg md:text-xl text-gray-400">
           <Truck className="text-gray-400" />
           Frete grátis para todo o Brasil
         </p>
